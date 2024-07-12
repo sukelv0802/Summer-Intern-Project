4c6a8db0b3df9ea73fff4f3743257398e61039ce
ch9992 <chenyang.hu@gentex.com>
2024-07-12 14:57:42 -0400

    enable fast switching

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ adc = ADC(Pin(27))  # Assuming GPIO 27 is ADC capable
 
 mux_en_pins = [Pin(i, Pin.OUT) for i in range(8, 8 + mux_num)]
 wr_pin = Pin(20, Pin.OUT)
+en_pin = Pin(21, Pin.OUT)
 temp_pin = machine.ADC(4)
 
 # Configure GPIOA pins as outputs
@@ -33,12 +34,12 @@ def setup_mcp23017():
 # Enable mux
 def enable_mux(mux_index):
     for i, pin in enumerate(mux_en_pins):
-        pin.value(1 if i != mux_index else 0)
+        pin.value(0 if i != mux_index else 1)
 
 # Disable mux
 def disable_all_muxes():
     for pin in mux_en_pins:
-        pin.value(1)
+        pin.value(0)
 
 # Reset mux
 def reset_mux():
@@ -47,9 +48,9 @@ def reset_mux():
 # Select the multiplexer channel
 def select_channel(channel):
     if 0 <= channel <= 31:
-        wr_pin(0)
+        wr_pin.value(0)
         i2c.writeto_mem(MCP23017_ADDR, GPIOA, bytes([channel]))
-        wr_pin(1)
+        wr_pin.value(1)
     else:
         print("Invalid Channel")
 
@@ -65,42 +66,28 @@ def find_potentiometer():
     potentiometers = []
     for mux_index in range(len(mux_en_pins)):
         for channel in range(32):
-            check_for_pause()
             temp_adc_value = temp_pin.read_u16()
             temp = adc_to_temp(temp_adc_value)
             select_channel(channel)
             enable_mux(mux_index)
-            time.sleep(0.5)  # Allow the multiplexer to settle and ADC to stabilize
+            en_pin.value(0)
             adc_value = read_adc()
             voltage = (adc_value / 65535) * 3.3
             data = f"Mux: {mux_index + 1}  Channel: {channel + 1}  Temperature: {temp:.5f}  Voltage: {voltage:.4f}"
             print(data)
+            time.sleep(0.1)  # Allow the multiplexer to settle and ADC to stabilize
             uart.write(data.encode())
             if adc_value > threshold:  # Define a suitable threshold based on your setup          
                 potentiometers.append((mux_index + 1, channel + 1))
             disable_all_muxes()
+            en_pin.value(1)
     return potentiometers
-
-def check_for_pause():
-    if uart.any():
-        command = uart.read().decode('utf-8').strip()
-        if command == 'PAUSE':
-            print("Pausing...")
-            uart.write(b'Pause confirmed\n')
-            while True:
-                if uart.any():
-                    command = uart.read().decode('utf-8').strip()
-                    if command == 'RESUME':
-                        print("Resuming...")
-                        uart.write('Resume confirmed\n')
-                        break
 
 # Main execution
 setup_mcp23017()
 reset_mux()
 threshold = 15000  # Example threshold, adjust based on potentiometer's expected ADC output
 while True:
-    check_for_pause()
     pot_channels = find_potentiometer()
     # if pot_channels:
     #     for mux, channel in pot_channels:
